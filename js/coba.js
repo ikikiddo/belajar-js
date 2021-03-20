5e35d6ef30fe8ed6945ebe65ebd36b5c62a06830
ikikiddo <79526358+ikikiddo@users.noreply.github.com>
2021-03-20 11:44:21 +0700

    Menambah Folder JS

---
+++ js/coba.js
@@ -0,0 +1,8 @@
+
+        document.writeln("Hello World");
+        document.write("<h1>  Kiddo </h1>")
+        document.write("Lorem*200")
+        document.write("</br>")
+            let a='Iki'+' '
+            const b="Kiddo"
+            document.write(a+b)
